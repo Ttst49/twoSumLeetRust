3eab54d0ca99c864ffc9f59f3bb189e3255835bc
Thibaut Stachnick <thibautstachnick@gmail.com>
2024-05-25 16:15:57 +0200

    submit a solution using hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+
+
+
+pub fn two_sum(nums: Vec<i32>, target: i32)->Vec<i32>{
+    use std::collections::HashMap;
+
+    let mut m: HashMap<i32, i32> = HashMap::new();
+    for (i, v) in nums.iter().enumerate() {
+        match m.get(&(target - *v)) {
+            Some(&j) => return vec![i as i32, j],
+            None => m.insert(*v, i as i32),
+        };
+    }
+    vec![]
+}
+
 fn main() {
-    println!("Hello, world!");
+    let nums: Vec<i32> = vec![3,3];
+    let target: i32 = 6;
+    let result = two_sum(nums,target);
+    if !result.is_empty() {
+        println!("[{}][{}]",result[0],result[1])
+    }else { println!("No answer") }
 }
